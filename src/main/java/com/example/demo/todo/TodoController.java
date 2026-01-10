15fc1d51d967c9932e24e265fcff2d0149dc0c21
jun-nakaura <julieeee1001@outlook.jp>
2026-01-11 00:07:57 +0900

    Add PageController and separate API routes

--- src/main/java/com/example/demo/todo/TodoController.java
+++ src/main/java/com/example/demo/todo/TodoController.java
@@ -1,33 +1,44 @@
 package com.example.demo.todo;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
-
-
 @RestController
-@CrossOrigin(origins = "*") 
+@CrossOrigin(origins = "*")
 @RequestMapping("/api/todos")
 public class TodoController {
+
     private final TodoService service;
-    public TodoController(TodoService service) { this.service = service; }
+
+    public TodoController(TodoService service) {
+        this.service = service;
+    }
 
     @GetMapping
-    public List<Todo> list() { return service.findAll(); }
+    public List<Todo> list() {
+        return service.findAll();
+    }
 
     @GetMapping("/{id}")
-    public Todo get(@PathVariable Long id) { return service.findById(id); }
+    public Todo get(@PathVariable Long id) {
+        return service.findById(id);
+    }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Todo create(@RequestBody Todo t) { return service.create(t); }
+    public Todo create(@RequestBody Todo t) {
+        return service.create(t);
+    }
 
     @PutMapping("/{id}")
-    public Todo update(@PathVariable Long id, @RequestBody Todo t) { return service.update(id, t); }
+    public Todo update(@PathVariable Long id, @RequestBody Todo t) {
+        return service.update(id, t);
+    }
 
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void delete(@PathVariable Long id) { service.delete(id); }
-
-    
+    public void delete(@PathVariable Long id) {
+        service.delete(id);
+    }
 }
